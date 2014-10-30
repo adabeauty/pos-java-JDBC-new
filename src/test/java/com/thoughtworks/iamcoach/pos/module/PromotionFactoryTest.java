5e059be6f31145acae8f3275485e4a45f47bc259
adabeauty <654296217@qq.com>
2014-10-31 00:27:50 +0800

    delete useless codes

--- src/test/java/com/thoughtworks/iamcoach/pos/module/PromotionFactoryTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/module/PromotionFactoryTest.java
@@ -4,9 +4,6 @@ import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
-/**
- * Created by wanghuan on 14-10-30.
- */
 public class PromotionFactoryTest {
     @Test
     public void can_generate_Promotion(){
