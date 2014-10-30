1cdd2274aa7c0aeb7f93df7dd6e3514de4f486c2
adabeauty <654296217@qq.com>
2014-10-31 00:27:26 +0800

    rename test name

--- src/test/java/com/thoughtworks/iamcoach/pos/module/PromotionFactoryTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/module/PromotionFactoryTest.java
@@ -9,7 +9,7 @@ import static org.fest.assertions.api.Assertions.assertThat;
  */
 public class PromotionFactoryTest {
     @Test
-    public void can_generate_BuyTwoOneFreePromotion(){
+    public void can_generate_Promotion(){
         assertThat(PromotionFactory.generatePromotion(1) instanceof BuyTwoOneFreePromotion).isTrue();
         assertThat(PromotionFactory.generatePromotion(2) instanceof SeconHalfPromotion).isTrue();
         assertThat(PromotionFactory.generatePromotion(3) instanceof DiscountPromotion).isTrue();
