12819643c5ccf3863fa33eba079858ae4f0f3db3
adabeauty <654296217@qq.com>
2014-11-01 20:43:48 +0800

    add before to PromotionServiceTest

--- src/test/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceTest.java
@@ -3,6 +3,7 @@ package com.thoughtworks.iamcoach.pos.service;
 import com.thoughtworks.iamcoach.pos.dao.*;
 import com.thoughtworks.iamcoach.pos.model.Promotion;
 import com.thoughtworks.iamcoach.pos.model.SeconHalfPromotion;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -11,19 +12,31 @@ import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 public class PromotionServiceTest {
+    PromotionService promotionService;
+
+    @Before
+    public void mock_PromotionImpl(){
+        int type = 1;
+        Promotion promotion = new SeconHalfPromotion();
+
+        ArrayList<Promotion> promotions = new ArrayList<Promotion>();
+        promotions.add(promotion);
+
+        PromotionDao promotionImple = mock(PromotionImple.class);
+        when(promotionImple.getPromotionByType(type)).thenReturn(promotion);
+        when(promotionImple.getPromotions()).thenReturn(promotions);
+
+        promotionService = new PromotionService(promotionImple);
+    }
+
     @Test
     public void can_get_promotion_by_type(){
-        PromotionService promotionService = new PromotionService();
-        promotionService.setPromotionImple(mock_PromotionImple());
         int type = 1;
         assertThat(promotionService.getPromotionByType(type) instanceof SeconHalfPromotion).isTrue();
     }
 
     @Test
     public void can_get_promotions(){
-        PromotionService promotionService = new PromotionService();
-        promotionService.setPromotionImple(mock_PromotionImple());
-
         assertThat(promotionService.getPromotions().size()).isEqualTo(1);
     }
 
