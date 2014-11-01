943b13b3450978248d13863d5662ff4ead456aec
adabeauty <654296217@qq.com>
2014-11-01 20:04:11 +0800

    transfer CartService to Printer

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,11 @@
+import com.thoughtworks.iamcoach.pos.service.CartService;
+
 public class App {
 
     public static void main(String[] args) {
-        Printer printer = new Printer();
+        CartService cartService = new CartService();
+
+        Printer printer = new Printer(cartService);
         System.out.println(printer.printList());
     }
 }
