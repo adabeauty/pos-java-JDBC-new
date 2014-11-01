49f556e4e041cb302b337aa91666397d74103dd4
adabeauty <654296217@qq.com>
2014-11-01 21:03:50 +0800

    add parament to CartService class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,17 @@
 import com.thoughtworks.iamcoach.pos.dao.ItemDao;
 import com.thoughtworks.iamcoach.pos.dao.ItemImple;
 import com.thoughtworks.iamcoach.pos.service.CartService;
+import com.thoughtworks.iamcoach.pos.util.ReadFileUlti;
+
+import java.util.ArrayList;
 
 public class App {
 
     public static void main(String[] args) {
+
+        ArrayList<String> inputs = ReadFileUlti.readFile("cart.txt");
         ItemDao itemImpl = new ItemImple();
-        CartService cartService = new CartService(itemImpl);
+        CartService cartService = new CartService(inputs, itemImpl);
 
         Printer printer = new Printer(cartService);
         System.out.println(printer.printList());
